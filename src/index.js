6cab70483c9946ca3f82e8321320b226615d3c65
Ensar Akkuzey <ensarakkuzey@gmail.com>
2023-01-05 00:32:28 +0300

    Create new jsx example file.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App(){
+    const inputType = "number"
+    const minValue = 3;
+
+    return (
+        <div>
+            <input type="number" min={5} max={10}/>
+            <br/>
+            <input type={inputType} min={minValue} max={10}/>
+        </div>
+
+    )
+}
+
+root.render(<App/>)
